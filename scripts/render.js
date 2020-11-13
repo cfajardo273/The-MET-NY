efc07820f3b2660e90c82507d241f8d5d5f1b32b
Pedro Martin <pataruco@gmail.com>
2020-11-13 15:30:57 +0000

    Render paitings

--- scripts/render.js
+++ scripts/render.js
@@ -1,12 +1,46 @@
+const template = document.querySelector('#painting');
+const paintingsSection = document.querySelector('#paintings');
+
 export const renderOnScreeen = (paintings) => {
   paintings.forEach((painting) => {
+    const {
+      title,
+      primaryImage,
+      artistDisplayName,
+      objectBeginDate,
+      medium,
+      dimensions,
+    } = painting;
+
     console.log({ painting });
+
+    const clone = template.content.cloneNode(true);
+
+    // image
+    const img = clone.querySelector('img');
+    img.setAttribute('alt', title);
+    img.setAttribute('src', primaryImage);
+
+    // title
+    const titleElement = clone.querySelector('#title');
+    titleElement.innerText = title;
+
+    // artist
+    const artist = clone.querySelector('#artist');
+    artist.innerText = artistDisplayName;
+
+    // date
+    const date = clone.querySelector('#date');
+    date.innerText = objectBeginDate;
+
+    // medium
+    const mediumElement = clone.querySelector('#medium');
+    mediumElement.innerText = medium;
+
+    // Dimensions
+    const dimensionsElement = clone.querySelector('#dimensions');
+    dimensionsElement.innerText = dimensions;
+
+    paintingsSection.appendChild(clone);
   });
-
-  const printOnScreen = (paintings) => console.log(paintings);
-
-
-  // TODO:
-
-  // Imprimir en pantalla (HTML) las pinturas
 };
